0a935b797b192083ffa37363ecd5d24aa3e3c2e7
ethereins <indahsyahfitri084@gmail.com>
2025-04-30 12:20:24 +0700

    pert2

---
+++ PrakSD04_1_2411532016/src/Pekan2/Buku.java
@@ -0,0 +1,12 @@
+package Pekan2;
+
+public class Buku {
+	String judul, pengarang,isbn;
+	Buku (String judul, String pengarang, String isbn){
+		this.judul = judul;
+		this.pengarang=pengarang;
+		this.isbn=isbn;
+		
+	}
+
+}
---
+++ PrakSD04_1_2411532016/src/Pekan2/Perpustakaan.java
@@ -0,0 +1,58 @@
+package Pekan2;
+import java.util.*;
+public class Perpustakaan {
+	LinkedList<Buku> koleksiBuku = new LinkedList<>();
+	Queue <Buku>Peminjaman = new LinkedList<>();
+	Stack <Buku>Pengembalian = new Stack<>();
+	//menggunakan linkedlist
+	void tambahBuku (String judul,String pengarang, String isbn) {
+		koleksiBuku.add(new Buku(judul,pengarang,isbn));
+	}
+//menggunakan queue
+	void pinjamBuku(String judul) {
+		for (Buku buku : koleksiBuku) {
+			if(buku.judul.equals(judul)) {
+				Peminjaman.add(buku);
+				break;
+			}
+		}
+	}
+	//menggunakan stack
+	void kembalikanBuku(String judul) {
+		for (Buku buku:Peminjaman) {
+			if (buku.judul.equals(judul)) {
+				Pengembalian.push(buku);
+				break;
+			}
+		}
+	}
+	public static void main(String[]args) {
+		Perpustakaan perpustakaan = new Perpustakaan();
+		Scanner scanner = new Scanner(System.in);
+		while(true) {
+			System.out.println("1. Tambah Buku\n2. Pinjam Buku\n3. Kembalikan Buku\n4. Keluar");
+			System.out.println("Pilih opsi");
+			int pilihan= scanner.nextInt();
+			scanner.nextLine();
+			if(pilihan==1) {
+				System.out.print("Masukkan judul:" );
+				String judul = scanner.nextLine();
+				System.out.print("Masukkan pengarang: ");
+				String pengarang = scanner.nextLine();
+				System.out.print("Masukkan ISBN: ");
+				String isbn = scanner.nextLine();
+				perpustakaan.tambahBuku(judul, pengarang, isbn);
+			}else if (pilihan==2) {
+				System.out.print("Masukkanjudul buku yang ingin dipinjam: ");
+				String judul = scanner.nextLine();
+				perpustakaan.pinjamBuku(judul);
+			}else if(pilihan==3) {
+				System.out.print("Masukkan jduk buku yang ingin dikembalikan: ");
+				String judul = scanner.nextLine();
+				perpustakaan.kembalikanBuku(judul);
+			}else if (pilihan==4) {
+				break;
+			}
+		}scanner.close();
+	}
+}
